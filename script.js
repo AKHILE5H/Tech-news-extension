18f061e84353b34ec03ff6ae61e037b32d6ab8df
AKHILE5H <82762852+AKHILE5H@users.noreply.github.com>
2022-05-21 13:21:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+fetch('https://gnews.io/api/v4/search?q=tech%20India&token=b1ebb86551fbb3c79b8a85a617f1227b&lang=en')
+    .then(data => data.json())
+    .then(newsData => {
+
+        //title
+        const totalNum =  10;
+        const articeNum = getRandomInt(totalNum);
+        const newsTitletext = newsData.articles[articeNum].title;
+        const newsTitle = document.getElementById('newsTitle');
+        document.getElementById("newsLink").href= newsData.articles[articeNum].url;
+        document.getElementById("newsLink").innerHTML="Link";
+        newsTitle.innerHTML = newsTitletext;
+
+        //image
+        const imgurl= newsData.articles[articeNum].image;
+        document.getElementById("newsImg").src=imgurl;
+
+        //description
+        const newsdesctext = newsData.articles[articeNum].description;
+        const newsDesc = document.getElementById('newsDesc');
+        newsDesc.innerHTML = newsdesctext;
+    })
+
